e68ea48641a42b2444bd88f0f8ddc199fc999cf3
YounesArifi <younesarifi@hotmail.com>
2021-06-27 18:54:07 +0200

    US-127 CRUD Notes + Tests + Validation

--- src/main/java/be/atc/salesmanagercrm/dao/impl/NotesDaoImpl.java
+++ src/main/java/be/atc/salesmanagercrm/dao/impl/NotesDaoImpl.java
@@ -2,6 +2,7 @@ package be.atc.salesmanagercrm.dao.impl;
 
 import be.atc.salesmanagercrm.dao.NotesDao;
 import be.atc.salesmanagercrm.entities.NotesEntity;
+import be.atc.salesmanagercrm.utils.EntityFinderImpl;
 
 import javax.persistence.EntityManager;
 import java.util.List;
@@ -9,39 +10,47 @@ import java.util.List;
 /**
  * @author Younes Arifi
  */
-public class NotesDaoImpl implements NotesDao {
+public class NotesDaoImpl extends EntityFinderImpl<NotesEntity> implements NotesDao {
     @Override
-    public void save(NotesEntity entity, EntityManager em) {
+    public void save(EntityManager em, NotesEntity entity) {
         em.persist(entity);
     }
 
     @Override
-    public NotesEntity findById(int id) {
-        return null;
+    public NotesEntity findById(EntityManager em, int id) {
+        return em.find(NotesEntity.class, id);
     }
 
     @Override
-    public List<NotesEntity> findNotesEntityByContactsByIdContacts(int id) {
-        return null;
+    public List<NotesEntity> findNotesEntityByContactsByIdContacts(EntityManager em, int id) {
+
+        return em.createNamedQuery("Notes.findNotesEntityByContactsByIdContacts",
+                NotesEntity.class)
+                .setParameter("id", id)
+                .getResultList();
     }
 
     @Override
-    public List<NotesEntity> findNotesEntityByCompagniesByIdCompagnies(int id) {
-        return null;
+    public List<NotesEntity> findNotesEntityByCompaniesByIdCompanies(EntityManager em, int id) {
+
+        return em.createNamedQuery("Notes.findNotesEntityByCompaniesByIdCompanies",
+                NotesEntity.class)
+                .setParameter("id", id)
+                .getResultList();
     }
 
     @Override
     public List<NotesEntity> findAll() {
-        return null;
+        return this.findByNamedQuery("Notes.findAll", new NotesEntity());
     }
 
     @Override
-    public void delete(int id) {
-
+    public void delete(EntityManager em, NotesEntity entity) {
+        em.remove(em.merge(entity));
     }
 
     @Override
-    public void update(NotesEntity entity) {
-
+    public void update(EntityManager em, NotesEntity entity) {
+        em.merge(entity);
     }
 }
