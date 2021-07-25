3b6f6143956c621e58107fe2917741f38c9e489c
YounesArifi <younesarifi@hotmail.com>
2021-07-25 13:04:15 +0200

    US-171 Ajout : Transaction Histories

--- src/main/java/be/atc/salesmanagercrm/dao/TransactionHistoriesDao.java
+++ src/main/java/be/atc/salesmanagercrm/dao/TransactionHistoriesDao.java
@@ -3,6 +3,7 @@ package be.atc.salesmanagercrm.dao;
 import be.atc.salesmanagercrm.entities.TransactionHistoriesEntity;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 /**
  * @author Younes Arifi
@@ -11,4 +12,5 @@ public interface TransactionHistoriesDao {
 
     void save(EntityManager em, TransactionHistoriesEntity entity);
 
+    List<TransactionHistoriesEntity> findAllByIdUserAndByIdTransaction(EntityManager em, int idTransaction, int idUser);
 }
---
+++ src/test/java/be/atc/salesmanagercrm/beans/TransactionHistoriesBeanTest.java
@@ -0,0 +1,67 @@
+package be.atc.salesmanagercrm.beans;
+
+import be.atc.salesmanagercrm.entities.TransactionHistoriesEntity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+class TransactionHistoriesBeanTest {
+
+    @Getter
+    @Setter
+    private TransactionHistoriesBean transactionHistoriesBean;
+
+    @BeforeEach
+    public void init() {
+        log.info("Appel avant chaque test");
+        transactionHistoriesBean = new TransactionHistoriesBean();
+    }
+
+    @AfterEach
+    public void after() {
+        log.info("Appel après chaque test");
+        transactionHistoriesBean = null;
+    }
+
+    @Test
+    void findAllByIdUserAndByIdTransactionShouldReturnTrue() {
+
+        // Mettre un id incorrect
+        int idTransaction = 13;
+        int idUser = 1;
+
+        List<TransactionHistoriesEntity> transactionHistoriesEntities = transactionHistoriesBean.findAllByIdUserAndByIdTransaction(idTransaction, idUser);
+
+
+        boolean test = !transactionHistoriesEntities.isEmpty();
+
+        log.info("Le test vaut : " + test);
+
+        assertThat(test).isEqualTo(true);
+    }
+
+    @Test
+    void findAllByIdUserAndByIdTransactionShouldReturnFalse() {
+
+        // Mettre un id correct
+        int idTransaction = 1540;
+        int idUser = 1;
+
+        List<TransactionHistoriesEntity> transactionHistoriesEntities = transactionHistoriesBean.findAllByIdUserAndByIdTransaction(idTransaction, idUser);
+
+
+        boolean test = !transactionHistoriesEntities.isEmpty();
+
+        log.info("Le test vaut : " + test);
+
+        assertThat(test).isEqualTo(false);
+    }
+}
